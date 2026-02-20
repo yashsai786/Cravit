46dee55bf70a90db5e73d7f750542561789e2be2
Yash Gangwani <yashgangwani786@gmail.com>
2026-02-20 23:45:00 +0530

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold">Cravit Project Started</h1>
+        </div>
+    );
+};
+
+export default App;
